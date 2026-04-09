779138031c4e9bc6ce829b7c2a15104fec0adf3a
shaliq-rahman <149221172+shaliq-rahman@users.noreply.github.com>
2026-04-09 15:56:48 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
+    // Your project has ESLint errors.
+    // Fix
     ignoreDuringBuilds: true,
   },
 };
